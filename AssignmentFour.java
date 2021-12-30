64ccb64ca6ea8c99eefb4159c1dbead79d3aa611
Karthick-vivek <96713556+Karthick-vivek@users.noreply.github.com>
2021-12-30 18:10:17 +0530

    Update AssignmentFour.java

--- AssignmentFour.java
+++ AssignmentFour.java
@@ -3,6 +3,7 @@ package seleniumBasics;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 public class AssignmentFour {
 
@@ -22,16 +23,22 @@ public class AssignmentFour {
 
 		driver.findElement(By.xpath("//button[text()='GOT IT']")).click();
 		Thread.sleep(10000);
-		
+
 		System.out.println("Handled cookies pop-up");
 
 		String text1 = driver.findElement(By.xpath("//p[@class='caption']")).getText();
 
 		System.out.println("Header text: "+ text1);
 
+		Assert.assertEquals(text1,"Share your location with Naukri.com for more relevant jobs","Alert text is not matching");
+
+
 		String text2 = driver.findElement(By.xpath("//p[@class='desc']")).getText();
 
-		System.out.println("Sub text: "+ text2);
+		System.out.println("Sub text: "+ text2); 
+
+		Assert.assertEquals(text2,"You can turn them off anytime from browser settings","Alert text is not matching");
+
 
 		driver.findElement(By.xpath("//span[contains(text(), 'Later')]")).click();
 		Thread.sleep(2000);
@@ -43,8 +50,6 @@ public class AssignmentFour {
 		driver.quit();
 
 		System.out.println("Run Successful");
-
-
 
 	}
 
