e9a2b022d1035c854088075ae851fb06747bca3f
Karthick-vivek <96713556+Karthick-vivek@users.noreply.github.com>
2021-12-30 15:38:40 +0530

    Add files via upload

---
+++ AssignmentThree.java
@@ -0,0 +1,66 @@
+package seleniumBasics;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AssignmentThree {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+
+		System.setProperty("webdriver.chrome.driver", "C:\\SeleniumBrowserDriver\\chromedriver.exe");
+
+		WebDriver driver = new ChromeDriver();
+
+		driver.get("http://zero.webappsecurity.com/");
+
+		System.out.println("****Launched the browser****");
+
+		driver.manage().window().maximize();
+
+		driver.findElement(By.id("signin_button")).click();
+
+		System.out.println("Clicked sigin button");
+
+		driver.findElement(By.cssSelector("input#user_login")).sendKeys("username");
+
+		driver.findElement(By.xpath("//input[contains(@name, 'user_password')]")).sendKeys("password");
+
+		driver.findElement(By.name("submit")).click();
+
+		System.out.println("Signed in");
+
+		driver.findElement(By.id("details-button")).click();
+		Thread.sleep(1000);
+
+		driver.findElement(By.xpath("//a[@id='proceed-link']")).click();
+
+		driver.findElement(By.linkText("Pay Bills")).click();
+
+		driver.findElement(By.linkText("Purchase Foreign Currency")).click();
+		Thread.sleep(1000);
+
+		driver.findElement(By.id("purchase_cash")).click();
+
+		Alert alert = driver.switchTo().alert();
+
+		System.out.println(alert.getText());
+
+		alert.accept();
+
+		driver.findElement(By.xpath("(//a[@class='dropdown-toggle'])[2]")).click();
+		
+		driver.findElement(By.cssSelector("a#logout_link")).click();
+
+		System.out.println("***Run Successful***");
+		driver.close();
+
+		driver.quit();
+
+
+
+	}
+
+}
